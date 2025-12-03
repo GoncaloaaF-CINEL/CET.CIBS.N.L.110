9c544d4cad3a80b1b9dbe918e8019abdd8aafd5f
GoncaloaaF <goncaloaaf@gmail.com>
2025-12-03 20:54:06 +0000

    demo print

---
+++ UC00245 - Desenvolver algoritmos/01_intro/main.py
@@ -0,0 +1,38 @@
+# comt
+
+"""
+comt
+com
+mais
+de
+uma
+linha
+"""
+
+"""
+/n -> muda de linha
+/t - Tab
+/f - paragrafo
+
+\" - > "
+\\ -> \
+"""
+
+# mostrar msg ao user
+
+print("ola\nMundo")
+print("ola\tMundo 2")
+print("ola\fMundo 3")
+
+# ola Mundo 3: "Gonçalo"
+print(' ola Mundo 3: "Gonçalo" ')
+
+print("ola Mundo 3: \"Gonçalo\"")
+
+# receber inputs do user
+
+
+# var
+
+
+# if
