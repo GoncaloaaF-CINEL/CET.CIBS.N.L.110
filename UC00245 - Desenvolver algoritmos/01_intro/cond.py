4044f44c7096b63b364c9460f98d77686be8055d
GoncaloaaF <goncaloaaf@gmail.com>
2025-12-03 22:48:12 +0000

    pre demo final

---
+++ UC00245 - Desenvolver algoritmos/01_intro/cond.py
@@ -0,0 +1,31 @@
+# ler valor 1
+
+valor1 = int(input("Digite um valor: "))
+
+# ver se ele e igual a 10
+
+if valor1 == 10:
+    print("Sim")  # corre quando a condição é verdadeira
+    # pass  # Cria um bloco vazio, usado para não dar erro
+else:
+    print("Não")  # corre quando a condição não é verdadeira
+    # pass  # Cria um bloco vazio, usado para não dar erro
+
+# crie uma app que:
+
+# peça 2 números inteiros ao utilizado
+
+num1 = int(input("Digite o valor do 1º num: "))
+num2 = int(input("Digite o valor do 2º num: "))
+
+# mostrar o maior dos dois num, se forem iguais mostre um deles
+
+# if que verifica qual o maior
+
+
+# mostrar o menor dos dois num, se forem iguais mostre um deles
+
+
+# verifique se os números são iguais,
+# se forem mostre o dobro da soma, se forem diferentes mostre metade da soma
+# não faça a operação no print (deve guardar os valores em variáveis)
--- UC00245 - Desenvolver algoritmos/01_intro/main.py
+++ UC00245 - Desenvolver algoritmos/01_intro/main.py
@@ -29,10 +29,53 @@ print(' ola Mundo 3: "Gonçalo" ')
 
 print("ola Mundo 3: \"Gonçalo\"")
 
-# receber inputs do user
-
-
 # var
 
+nome = "Gonçalo"  # String -> str
+print(nome)
+
+nome = 12
+print(nome)
+"""
+
+int - inteiro 
+float - decimal
+str - texto 
+bool - T / F
+
+"""
+
+# type hint
+idade: int = 20
+print(idade)
+
+idade = 23
+print(idade)
+
+idade = "Gonçalo"
+print(idade)
+
+# receber inputs do user
+
+nova_var_com_nome_cool = input("nome: ")  # devolve sempre str
+print(nova_var_com_nome_cool)
+
+# faça uma app py linha de comandos que some 2 números inteiros
+
+num1 = int(input("num1: "))
+num2 = int(input("num2: "))
+soma = num1 + num2
+
+print("a soma é", soma)
+print("a soma é " + str(soma))
+print(f"a soma é {soma}")
+
+print(f"a soma de {num1} com {num2} é {soma}")
+
+nome = input("Digite o nome: ")
+print(f"o nome digitado foi: {nome}")  # f-string
 
 # if
+
+
+# while
