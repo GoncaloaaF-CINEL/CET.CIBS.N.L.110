23bb071b3e06a4810fb557c1ccd7fd70e5655428
GoncaloaaF <goncaloaaf@gmail.com>
2026-04-29 23:06:16 +0100

    fim da aula

--- UC00606 - Desenvolver programas em linguagem estruturada/14_classes/Carro.py
+++ UC00606 - Desenvolver programas em linguagem estruturada/14_classes/Carro.py
@@ -14,7 +14,10 @@ class Carro:
 
     """
 
-    marca = ""
-    modelo = ""
-    ano = 0
+    def __init__(self, marca, modelo, ano):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
 
+    def mudar_ano(self, novo_ano):
+        self.ano = novo_ano
--- UC00606 - Desenvolver programas em linguagem estruturada/14_classes/main.py
+++ UC00606 - Desenvolver programas em linguagem estruturada/14_classes/main.py
@@ -1,11 +1,14 @@
 from Carro import Carro
 
-c = Carro()
+c = Carro("BMW", "320D", 2026)
 
 print(c.marca)
 print(c.modelo)
 print(c.ano)
 
+print("----")
+
+"""
 c.marca = "BMW"
 c.modelo = "320D"
 c.ano = 2026
@@ -13,12 +16,14 @@ c.ano = 2026
 print(c.marca)
 print(c.modelo)
 print(c.ano)
+"""
 
-c2 = Carro()
+c2 = Carro("Audi", "R8", 2026)
 print(c2.marca)
 print(c2.modelo)
 print(c2.ano)
 
+"""
 c2.marca = "Audi"
 c2.modelo = "R8"
 c2.ano = 2026
@@ -26,9 +31,25 @@ c2.ano = 2026
 print(c2.marca)
 print(c2.modelo)
 print(c2.ano)
+"""
+print("----")
 
+c3 = Carro("Shelby", "GT500", 1967)
+print(c3.marca)
+print(c3.modelo)
+print(c3.ano)
+
+print("----")
+print("----")
+
+print(c2.ano)
+c2.mudar_ano(2020)
+print(c2.ano)
+
+print("----")
+print("----")
 print("------")
-lst = [c, c2]
+lst = [c, c2, c3]
 
 for i in lst:
     print(i.marca)
@@ -41,6 +62,10 @@ Classe Bola: Crie uma classe que modele uma Bola:
         - cor,
         - circunferência,
         - material
+
+    Métodos:
+        - trocaCor
+        - mostraCor
 
 """
 
